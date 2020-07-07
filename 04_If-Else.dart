10f71ba248495a97d46b82f1bfa47f7a61386ca2
JanH00 <67894980+JanH00@users.noreply.github.com>
2020-07-07 13:55:43 +0200

    Create 04_If-Else.dart

---
+++ 04_If-Else.dart
@@ -0,0 +1,17 @@
+void main() {
+  
+int zahlengrenze = 5;
+  
+if(zahlengrenze > 10) {
+  print("Y");
+}
+  
+else if(zahlengrenze == 5) {
+    print("N");
+ }
+ else {
+   print("OK");
+ }
+  
+  
+}
